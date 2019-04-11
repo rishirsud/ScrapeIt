7a4efd2f86485540c0929f85e0710251f9be2b66
Jonah <jonahkarew@gmail.com>
2019-04-11 11:52:21 -0400

    commit for pull

--- assets/js/createplaylist.js
+++ assets/js/createplaylist.js
@@ -1,5 +1,5 @@
 // get userID
-
+// GET https://api.spotify.com/v1/me
 
 
 // POST https://api.spotify.com/v1/users/{user_id}/playlists
---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+//on pageload request spotify login details, authenticate
+
+
+
+// make api request to reddit to request title data
