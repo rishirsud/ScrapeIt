714309765e57c6e75da3bd3e35bbdf4792c398c0
Rishi S <rishirsud@gmail.com>
2022-05-23 15:41:43 -0400

    Update addingSpotifyAuthentication.js

--- assets/js/addingSpotifyAuthentication.js
+++ assets/js/addingSpotifyAuthentication.js
@@ -1,4 +1,4 @@
-const spotify_CLIENT = "92a0946328c54acba24e465dbfd53650";
+// const spotify_CLIENT = "92a0946328c54acba24e465dbfd53650";
 // const spotify_CLIENT = "bec3519af734411885a23e2440879e5c";
 const spotify_CLIENT = "a192be5079c84f01bc02bb156407c210";
 
